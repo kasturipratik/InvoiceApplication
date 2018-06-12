a505f5279d1fc3c2392f2c5b5c1e609ad0112981
kasturipratik <pratik.parajuli@yahoo.com>
2018-06-12 09:36:40 -0400

    commented

--- src/com/company/Invoice.java
+++ src/com/company/Invoice.java
@@ -3,12 +3,17 @@ package com.company;
 import java.util.ArrayList;
 
 public class Invoice {
-
+/*
+    private variable for invoice method
+*/
     private String item;
     private int quantity;
     private String description;
     private double price;
 
+ /*
+    getter and setters for the variables
+ */
     public String getItem() {
         return item;
     }
@@ -40,11 +45,17 @@ public class Invoice {
     public void setPrice(double price) {
         this.price = price;
     }
-
+/*
+    calculating the total of individual item using quantity and price
+*/
     public double total(){
         double total = getQuantity()*getPrice();
         return total;
     }
+
+ /*
+    calculating the taxable amount by adding all the item total
+ */
     public double taxableSubTotal(ArrayList<Double> subTotal){
         double taxable = 0.0;
         for(int i =0; i <subTotal.size();i++){
@@ -52,16 +63,29 @@ public class Invoice {
         }
         return taxable;
     }
+  /*
+   returning the untaxable amount
+  */
     public double untaxable(){
         return 0;
     }
+
+    /*
+        calculating the tax of the subtotal amount
+    */
     public double tax(ArrayList<Double> subTotal){
         double taxRate =6.0;
         double tax = (taxRate * taxableSubTotal(subTotal))/100;
         return tax;
     }
+
+    /*
+        calculating the grandtotal of items entered by the users
+    */
     public String grandTotal(ArrayList<Double> subTotal){
+        //adding subtotal and tax
         double total = tax(subTotal)+ taxableSubTotal(subTotal);
+        // formating the grand total to print 2 decimal places
         String grandTotal = String.format("%.02f",total);
         return grandTotal;
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,47 +1,57 @@
 package com.company;
 
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// variable declaration
         int quantity =0;
         String description="", item="";
         double price ,total;
 
         Scanner input = new Scanner(System.in);
-
+/*
+    array list for storing the user inputs
+*/
         ArrayList<Double> totalStore = new ArrayList<>();
         ArrayList<Double>priceStore = new ArrayList<>();
         ArrayList<String> descriptionStore = new ArrayList<>();
         ArrayList<Integer> quantityStore = new ArrayList<>();
         ArrayList<String> itemStore = new ArrayList<>();
 
-
+// instantiate  invoice class
         Invoice invoice = new Invoice();
 
         p("Enter the item number of the product or q to exit");
         item = input.nextLine();
 
         while (!item.equals("q")){
-
+/*
+    user input
+*/
             p("Enter the Quantity of the product: ");
             quantity=input.nextInt();
             input.nextLine();
+
             p("Enter the Description of the product: ");
             description= input.nextLine();
+
             p("Enter the Price of the product: ");
             price = input.nextDouble();
             input.nextLine();
+/*
+    setting the data to the variable of the invoice method
+*/
             invoice.setItem(item);
             invoice.setQuantity(quantity);
             invoice.setDescription(description);
             invoice.setPrice(price);
-
+/*
+    Adding the data to the array list
+*/
             totalStore.add(invoice.total());
             quantityStore.add(invoice.getQuantity());
             itemStore.add(invoice.getItem());
@@ -52,19 +62,27 @@ public class Main {
             p("Enter the Next item number of the product or q to exit");
             item = input.nextLine();
         }
-
+/*
+    printing the stored data from the array list
+*/
         p("Item  | Quantity  | Description | Price  | Total  ");
         for(int i =0; i <totalStore.size();i++){
             p(itemStore.get(i)+"    "+quantityStore.get(i)+"    "+descriptionStore.get(i)+"    "+
                     priceStore.get(i)+"    "+totalStore.get(i));
         }
 
+        /*
+            printing the calculations done using methods
+        */
         p("Taxable sub-total: " + invoice.taxableSubTotal(totalStore));
         p("Untaxable Subtotal: " + invoice.untaxable());
         p("Tax: "+ invoice.tax(totalStore));
         p("Grand Total: "+ invoice.grandTotal(totalStore));
 
     }
+    /*
+        printing statement for easier use
+    */
     public static void p(String s){
         System.out.println(s);
     }
